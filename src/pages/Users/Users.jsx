cda9d290b148415695b25e9bdc9ced7012487f26
ruth-zon <ruthzon711@gmail.com>
2023-05-15 17:00:29 +0300

    user details

--- src/pages/Users/Users.jsx
+++ src/pages/Users/Users.jsx
@@ -1,17 +1,18 @@
 import React, { useEffect, useState } from "react";
 import { Link, useNavigate } from "react-router-dom";
+import Modal from "../../components/modal";
 import config from "../../config";
 import useCookie from "../../utils/useCookie";
 import style from "./style";
 
 const Users = () => {
   const [users, setUsers] = useState([]);
+  const [showModal, setShowModal] = useState();
   const navigate = useNavigate();
   const [token, setToken] = useCookie("access_token");
   const username = window.localStorage.getItem("username");
 
   const getUsers = async (token) => {
-    console.log("token", token);
     let response = await fetch(`${process.env.REACT_APP_BACKEND_URL}/user`, {
       headers: {
         "Content-Type": "application/json",
@@ -44,6 +45,20 @@ const Users = () => {
       navigate("/login");
     }
   };
+
+  const openModal = async (username) => {
+    let response = await fetch(
+      `${process.env.REACT_APP_BACKEND_URL}/user/${username}`,
+      {
+        headers: {
+          "Content-Type": "application/json",
+          Authorization: "Bearer " + getItem("access_token"),
+        },
+      }
+    );
+    response = await response.json();
+    setShowModal(response);
+  };
   return (
     <div>
       <style.Header>
@@ -62,7 +77,7 @@ const Users = () => {
         ) : null}
         {users.length
           ? users.map((user) => (
-              <tr key={user.username}>
+              <tr key={user.username} onClick={() => openModal(user.username)}>
                 {Object.keys(users[0]).map((key) => (
                   <td key={key}>{user[key]}</td>
                 ))}
@@ -70,6 +85,16 @@ const Users = () => {
             ))
           : null}
       </style.Table>
+      <Modal onClose={() => setShowModal(false)} show={showModal}>
+        {showModal ? (
+          <div>
+            <h1>{showModal.username}</h1>
+            <h6>{"user agent " + showModal.userAgent}</h6>
+            <h6>{"register time " + showModal.createdAt.toString()}</h6>
+            <h6>{"logins count " + showModal.loginsCount}</h6>
+          </div>
+        ) : null}
+      </Modal>
     </div>
   );
 };
